078c4b3d57638c230dc15c69c7f75a7e304a99a0
Z_green_Z <valdisvan@mail.ru>
2022-02-07 16:43:37 +0300

    Update GetPerson.java

--- EyesTrek/src/Pojo/GetPerson.java
+++ EyesTrek/src/Pojo/GetPerson.java
@@ -5,23 +5,13 @@
 //
 //______________________________________________________
 
-
-
-
 package Pojo;
 
-
-
-
-
 public class GetPerson {
-    
            public String [] descriptors;
            public String   create_time;
            public String   user_data;
            public String id; 
-           public String [] lists;
-           
+           public String [] lists; 
            public GetPerson(){}
-    
 }
