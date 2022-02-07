0351e8f10c17ff88c549dae951fe0e27eadd0450
Z_green_Z <valdisvan@mail.ru>
2022-02-07 16:25:45 +0300

    Update HMI.java

--- EyesTrek/src/HMI/HMI.java
+++ EyesTrek/src/HMI/HMI.java
@@ -26,104 +26,58 @@ import java.net.URL;
 public class HMI {
 
     
-    public static int x=300,     //ширина Фрэйма
-                      y=500 ;   //высота
-    
-
-  
+  public static int x=300,     //ширина Фрэйма
+                    y=500 ;   //высота
   public static final String ICON_STR = "logo2.png"; 
-  
-  
   public static SetFrame runF;              //  экземпляр класса фрейма
-    
   public static boolean tr = true;          // Тригер фрейма
   
 //****************************************************************************** 
   
-    
 
-  
-
-    
 // Интерфейс иконки
-    
  public static void setTrayIcon() throws Exception  {
     if(! SystemTray.isSupported() ) {
       return;
     }
-    
-   
-    
-    
-
+     
+     
     PopupMenu trayMenu = new PopupMenu();
     MenuItem item = new MenuItem("Exit");
     MenuItem item2 = new MenuItem("Settings");
-   
-    
-    
-    
     
     item.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        System.exit(0);
-        
-      }
-      
-      
-    }); 
-    
-    
+        System.exit(0);    
+      }    
+    });    
     trayMenu.add(item);
    //-------------------------------------------------------------
-   
-   
-   
-   
-   
-    
-   
+  
     item2.addActionListener(new ActionListener() {
       @Override
-      public void actionPerformed(ActionEvent e) {
-       
-          if(tr)runF = new SetFrame();
-     
-       
-      }
-        
-    }); 
-    
-    
+      public void actionPerformed(ActionEvent e) { 
+          if(tr)runF = new SetFrame();     
+      }       
+    });   
     trayMenu.add(item2);
     //-------------------------------------------------------------
    
-   
-
     URL imageURL = HMI.class.getResource(ICON_STR);
-
     Image icon = Toolkit.getDefaultToolkit().getImage(imageURL);
     TrayIcon trayIcon = new TrayIcon(icon, APPLICATION_NAME, trayMenu);
     trayIcon.setImageAutoSize(true);
-
     SystemTray tray = SystemTray.getSystemTray();
+     
     try {
       tray.add(trayIcon);
     } catch (AWTException e) {
     }
 
     trayIcon.displayMessage(APPLICATION_NAME, "Open programm!",
-                            TrayIcon.MessageType.INFO);
-    
-    
-    
-//------------------------------------------------------------------------------    
+                            TrayIcon.MessageType.INFO);    
   }
- 
- 
-
-
  
 //==============================================================================
 //END
